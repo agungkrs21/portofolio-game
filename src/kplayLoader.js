5919f87518148df8eb281d96ce1793e214f44536
agungkrx <47ungkrs@gmail.com>
2026-06-24 18:22:13 +0800

    add player sprite

--- src/kplayLoader.js
+++ src/kplayLoader.js
@@ -4,12 +4,25 @@ const scale = 4;
 
 export const k = kaplay({
   letterBox: true,
-  // background: '#d46eb3',
+  background: '#000000',
   scale,
   canvas: document.getElementById('game'),
   global: false,
 });
 
+k.loadSprite('char_robot', '../asset/mainChar/Char_Robot.png', {
+  sliceX: 8,
+  sliceY: 6,
+  anims: {
+    idle: { from: 16, to: 18, speed: 7, loop: true },
+    shoot: { from: 32, to: 37, loop: true },
+    jump: { from: 24, to: 29, loop: true },
+    walk: { from: 8, to: 15, loop: true },
+    walkShoot: { from: 0, to: 7, loop: true },
+    heal: { from: 19, to: 24, loop: true },
+    death: { from: 40, to: 45, loop: true },
+  },
+});
 k.loadSprite('intro', '../maps/intro.png');
 k.loadSprite('border', '../asset/border/corder-edge.png', {
   sliceX: 2,
--- src/scene/intro.js
+++ src/scene/intro.js
@@ -2,12 +2,11 @@ import { drawBorder } from '../ui/drawBorder.js';
 
 export function intro(k) {
   drawBorder(k.width(), k.height());
-  const map = k.add([k.pos(0, 0), k.sprite('intro')]);
+  // const map = k.add([k.pos(0, 0), k.sprite('intro')]);
 
-  const obj = map.add([
-    k.rect(32, 32), // Draw this object as a rectangle
-    k.pos(k.width() / 2, k.height() - (16 + 14)), // Position this object in X: 10 and Y: 20
-    'shape', // Classify this object as "shape"
+  const player = k.add([
+    k.pos(k.width() / 2, k.height() - 24),
+    k.sprite('char_robot', { anim: 'idle' }),
     k.anchor('center'),
   ]);
 }
